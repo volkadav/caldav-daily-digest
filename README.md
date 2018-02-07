bf5582d74c598da66462ffbed4da50d8d0ec7064
Michael Jackson <michael.o.jackson@gmail.com>
2018-02-07 11:05:56 -0800

    v0.0001.alpha-zomg-hax

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # caldav-daily-digest
-a little python script to put together a daily digest of events, e.g. to be run from cron to email to a user
+a little python script to put together a daily digest of events, e.g. to 
+be run from cron to email to a user
+
+# dependencies
+  - caldav
+  - icalendar
+
+(pip install works for both, but as a note, if you have caldav <= 0.5, 
+and your system's usernames include an "@", you will need to patch one file 
+by hand in line with this bug report:
+https://github.com/python-caldav/caldav/issues/11)
+
+optional but likely: a functioning cron setup to run this
+(e.g.: "30  6 * * 1-5 /path/to/caldav-daily-digest.py" to get a list of
+the day's meetings at 6:30am every workday)
+
+# tested on
+linux (debian, python 3.4, x86)
---
+++ caldav-daily-digest.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3.4
+
+"""calendar digest cron script
+
+this logs into a caldav server and tries to get a digest of all of the
+events scheduled for that day. naive and hacky but seems to mostly
+work? easiest way to run this is from cron.
+
+author: michael.o.jackson@gmail.com
+
+license: apache 2.0"""
+
+from datetime import datetime, timedelta
+from os import environ
+from urllib.parse import quote_plus
+from icalendar import Calendar
+import caldav
+import pytz
+
+# you'll probably want to change these:
+USERNAME = 'foobar@bazquux.com'
+PASSWORD = 'ChAng3M3!'
+BASE_URL = 'stbeehive.blahblahblah.foo/caldav/st/principals/individuals/'
+
+# below here you probably will not need to make changes
+URL = 'https://' + quote_plus(USERNAME) + ":" + quote_plus(PASSWORD) \
+        + '@' + BASE_URL + quote_plus(USERNAME) + '/'
+NOW = datetime.now()
+DAY_START = datetime(NOW.year, NOW.month, NOW.day)
+DAY_END = DAY_START + timedelta(hours=24)
+
+if "TZ" in environ:
+    LOCAL_TZ = pytz.timezone(environ["TZ"])
+else:
+    LOCAL_TZ = pytz.timezone("US/Pacific")
+
+def pretty_print_time(date_time):
+    """takes a datetime and normalizes it to local time, prints nicely"""
+    local_dt = date_time.replace(tzinfo=pytz.utc).astimezone(LOCAL_TZ)
+    return local_dt.strftime("%I:%M %p")
+
+print("pulling events for %d-%d-%d" % (NOW.year, NOW.month, NOW.day))
+
+CLIENT = caldav.DAVClient(URL)
+PRINCIPAL = CLIENT.principal()
+CALENDARS = PRINCIPAL.calendars()
+
+if not CALENDARS:
+    print("No calendars defined for " + USERNAME)
+else:
+    CALENDAR = CALENDARS[0]
+    EVENTS = CALENDAR.date_search(DAY_START, DAY_END)
+
+    if not EVENTS:
+        print("Nothing on your calendar for today!")
+    else:
+        FILTERED_EVENTS = []
+        for ev in EVENTS:
+            components = Calendar.from_ical(ev.data).walk('vevent')
+            if not components:
+                continue
+            FILTERED_EVENTS.append(components[0])
+
+        FILTERED_EVENTS.sort(key=lambda e: e.decoded('dtstart'))
+        for event in FILTERED_EVENTS:
+            summary = event.decoded('summary').decode('utf-8')
+            description = ""
+            if event.decoded('description') != b'':
+                description = event.decoded('description').decode('utf-8')
+            start = event.decoded('dtstart')
+            end = event.decoded('dtend')
+
+            print(u"%s - %s %s\n%s\n\n" % (pretty_print_time(start),
+                                           pretty_print_time(end),
+                                           summary,
+                                           description))
